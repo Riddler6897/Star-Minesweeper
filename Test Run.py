f072519b14dca64bca848794a60e9fcfaec36669
MLaCroix95 <matthew.brett.l95@gmail.com>
2017-05-05 09:40:14 -0500

    Update Test Run.py

--- Test Run.py
+++ Test Run.py
@@ -14,29 +14,32 @@ class Minesweeper(Frame):
         self.NoTile = []
         #Frame.__init__(self, master)
         self.master = master
-        
+#populates NoTile with images corresponding to the number of mines adjacent to each tile
         for x in range (1,9):
             self.NoTile.append(PhotoImage(file = "images/tile_"+str(x)+".gif"))
 
         self.button = []
-
-
-
+#arrays containing the position of each mine that will make the shape of the constellations
         self.Libra = [23, 63, 102, 124, 154, 161, 197, 209]
         self.Orion = [0, 2, 16, 30, 62, 66, 73, 89, 93, 103, 113, 118, 125, 132, 139, 146, 153, 211, 217]
         self.Phoenix = [22, 44, 45, 78, 85, 121, 148, 169, 203]
         self.Taurus = [3, 30, 35, 64, 67, 83, 95, 99, 113, 129, 146, 163, 178, 204, 223]
         self.CanisMajor = [21, 49, 69, 73, 81, 101, 140, 144, 186, 197, 203, 212]
         self.Draco = [32, 51, 63, 67, 96, 127, 157, 161, 167, 173, 193, 195, 198, 212, 224]
-        self.CanesVenatici = [43, 182]
+        self.CanesVenatici = [42, 182]
         self.Chameleon = [66, 104, 106, 133, 140]
         self.Crater = [4, 22, 98, 120, 142, 153, 164, 220]
         self.Vulpecula = [2, 51, 61, 83, 114, 145, 178, 209]
         self.levels = [self.CanesVenatici, self.Chameleon, self.Libra, self.Crater, self.Vulpecula, self.Phoenix, self.CanisMajor, self.Taurus, self.Draco, self.Orion]
-
-        self.level = 5
+#an array containing all the constellations in order of "difficulty"
+        self.level = 0
+        self.score = 0
+#labels showing the current level and the number of mines in the level        
         self.label1 = Label(master, text = "Mines: "+str(len(self.levels[self.level])))
-        self.label1.grid(row = 16, column = 0, columnspan = 15)
+        self.label1.grid(row = 16, column = 0, columnspan = 5)
+        self.label2 = Label(master, text = "Level: "+ str(self.score))
+        self.label2.grid(row = 16, column = 5, columnspan = 5)
+        
 
     def buttonPressed(self, i):
         self.button[i].config(image = self.images[1])
