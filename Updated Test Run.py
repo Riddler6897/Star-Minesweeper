a70146503abe84305ca02df7db847bc1360bf312
rolumbian <rolumbian@hotmail.com>
2017-05-09 20:45:57 -0500

    Add files via upload

--- Updated Test Run.py
+++ Updated Test Run.py
@@ -1,24 +1,38 @@
-from Tkinter import * #We could also use pygame#
-from time import time #To set a timer#
-from time import sleep #To make the RGBs turn on and off#
-#import RPi.GPIO as GPIO 
+##########################################################################################################################################
+##########################################################################################################################################
+# Names: Bradford Doughty, Steven Pirvu, Matthew LaCroix, Kimberly Atienza
+# Date: 5/01/17
+# Description: Final project for Compututer Science 132 at Louisiana Tech University Spring 2017. A variation of the classic game 
+#               Minesweeper with a constellation theme. 
+##########################################################################################################################################
+##########################################################################################################################################
+
+from Tkinter import * 
+from time import time #To set a timer
+from time import sleep #To make the RGBs turn on and off
+#import RPi.GPIO as GPIO
+
 class Minesweeper(Frame):
 
     def __init__(self, master):
 
+        # This imports the images from the images file for the game 
         self.images = [PhotoImage(file = "images/plaintile.gif"),
                        PhotoImage(file = "images/clickedtile.gif"),
                        PhotoImage(file = "images/minetile.gif"),
                        PhotoImage(file = "images/flagtile.gif")]
-        self.NoTile = []
+
+         #self.NoTile = []
         Frame.__init__(self, master)
         self.master = master
-#populates NoTile with images corresponding to the number of mines adjacent to each tile
+
+        #populates NoTile with images corresponding to the number of mines adjacent to each tile
         #for x in range (1,9):
             #self.NoTile.append(PhotoImage(file = "images/tile_"+str(x)+".gif"))
 
         self.button = []
-#arrays containing the position of each mine that will make the shape of the constellations
+
+        #arrays containing the position of each mine that will make the shape of the constellations
         self.Libra = [23, 63, 102, 124, 154, 161, 197, 209]
         self.Orion = [0, 2, 16, 30, 62, 66, 73, 89, 93, 103, 113, 118, 125, 132, 139, 146, 153, 211, 217]
         self.Phoenix = [22, 44, 45, 78, 85, 121, 148, 169, 203]
@@ -29,13 +43,16 @@ class Minesweeper(Frame):
         self.Chameleon = [66, 104, 106, 133, 140]
         self.Crater = [4, 22, 98, 120, 142, 153, 164, 220]
         self.Vulpecula = [2, 51, 61, 83, 114, 145, 178, 209]
+        #an array containing all the constellations in order of "difficulty"
         self.levels = [self.CanesVenatici, self.Chameleon, self.Libra, self.Crater, self.Vulpecula, self.Phoenix, self.CanisMajor, self.Taurus, self.Draco, self.Orion]
-#an array containing all the constellations in order of "difficulty"
+
+        # variables for the scoring system
         self.level = 0
         self.score = 1
         self.pressed = 0
         self.tiles = 225-len(self.levels[self.level])
-#labels showing the current level and the number of mines in the level        
+
+        #labels showing the current level and the number of mines in the level        
         self.label1 = Label(master, text = "Mines: "+str(len(self.levels[self.level])))
         self.label1.grid(row = 16, column = 0, columnspan = 5)
         self.label2 = Label(master, text = "Level: "+ str(self.score))
@@ -44,7 +61,7 @@ class Minesweeper(Frame):
         self.label3.grid(row = 16, column = 10, columnspan = 5)
         
         
-
+    # Changes the image when you press the tile in the game
     def buttonPressed(self, i):
         self.pressed += 1
         self.tiles -= 1
@@ -54,22 +71,24 @@ class Minesweeper(Frame):
         if self.tiles == 200:
             self.win()
 
+    # Makes you lose if you press a mine 
     def minePressed(self, i):
         self.button[i].config(image=self.images[2])
         #self.lose()
 
+    # Tells the user whether the block clicked is a mine or not by calling the mine Pressedfunction
     def constellation_plot(self):
         for i in self.levels[self.level]:
             self.button[i].config(command = lambda i=i: self.minePressed(i))
 
+    # Deletes the previous level when one level is finished
+    # Credit to user: martineau from Stackoverflow.com
     def grid_forget(self):
         while self.button:
             button = self.button.pop()
             button.destroy()
 
-    #def update_mine(self):
-        #pass
-
+    # A function to move user to next level once all tiles are found without clicking on a mine
     def win(self):
         self.grid_forget()
         self.score += 1
@@ -85,8 +104,8 @@ class Minesweeper(Frame):
         self.grid()
         
         
-        
-    def grid(self): #This will make the grid#
+    # Creates the grid and places the plain tile on each button
+    def grid(self): 
         i = 0
         for r in range(15):
             for c in range(15):
@@ -99,18 +118,11 @@ class Minesweeper(Frame):
         self.constellation_plot()
 
        
-    def play(): #Will actually run the game#
-        pass
         
     #def lose(self): #Will use GPIO for RGBs and allow the player to start the same level over#
         #quit(1)
         
-    
-    def time_out(): #timer. When the time goes out, the whole program will stop running while still showing score.#
-        pass
-        
-    def sound(): #To play a sound if they win or lose. Can be put under the lose function#
-        pass
+
 
 class Timer(Canvas):
     def __init__(self):
